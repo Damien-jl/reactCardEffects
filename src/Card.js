c88d814c7378ebded29a51f8a3373649b3d68892
Damien Legg <leggdamien@gmail.com>
2023-10-08 19:18:18 -0500

    completion of Card component

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,20 @@
 import React from "react";
 
+function Card({name, image}) {
+    const [{angle,x,y}] = useState({
+      angle: Math.random() * 90 - 45,
+      x: Math.random() * 40 - 20,
+      y: Math.random() * 40 - 20
+    })
+  
+    const change = `translate(${x}px, ${y}px) rotate(${angle})`;
+  
+    return <img
+        className="Card"
+        alt={name}
+        src={image}
+        style={change} 
+        />;
+  }
 
 export default Card;
